e39bb78dcf47cffcb1012baa1952aae6942584d7
rayannott <imjobjob@gmail.com>
2023-02-17 22:32:36 +0100

    added cycling through the colours

--- ui_elements.py
+++ ui_elements.py
@@ -1,7 +1,17 @@
 import pygame
 from utils import THEME, PANEL_HEIGHT
 import math
+from random import random, randint
 
+def hex_to_rgb(s):
+    s = s[1:]
+    return [int(s[i:i+2], 16) for i in (0, 2, 4)]
+
+def rgb_to_hex(rgb):
+    return '#%02X%02X%02X' % tuple(rgb)
+
+def random_color():
+    return rgb_to_hex((randint(0, 255),randint(0, 255),randint(0, 255)))
 
 
 class Button():
@@ -17,26 +27,36 @@ class Button():
         self.bg_color = bg_color
         self.text_placement_specifier = text_placement_specifier
         self.time = 0
+        self.dt = 1./240
+        self.color_time = 0
+
         self.pos_wiggle = [0, 0]
-        self.pos_wiggle_magn = min(self.size)*0.05
+        magnitude_t = random()
+        self.pos_wiggle_magn = min(self.size) * ((1-magnitude_t)*0.03 + magnitude_t*0.055)
+        self.rot_direction = (-1) ** (random() > 0.5)
+        rot_speed_t = random()
+        self.rs = (1-rot_speed_t) * 0.8 + rot_speed_t * 1.2 # rotation speed
+
+        # cycling button colours
+        self.col = THEME['def']
     
     def pos_wiggle_update(self):
-        self.pos_wiggle[0] = self.pos_wiggle_magn*math.sin(self.time)
-        self.pos_wiggle[1] = self.pos_wiggle_magn*math.cos(self.time)
+        self.pos_wiggle[0] = self.rot_direction * self.pos_wiggle_magn*math.sin(self.rs*self.time)
+        self.pos_wiggle[1] = self.pos_wiggle_magn*math.cos(self.rs*self.time)
 
 
     def draw(self, screen, font):
         def color_active(col):
             return col if self.is_active else THEME['button_inactive']
         if self.is_visible:
-            self.time += 1./240
+            self.time += self.dt
             x, y = self.topleft
             self.pos_wiggle_update()
             x += self.pos_wiggle[0]
             y += self.pos_wiggle[1]
             wi, he = self.size
             color = color_active(self.bg_color)
-            pygame.draw.rect(screen, THEME['def'], [x, y, wi, he], width=4)
+            pygame.draw.rect(screen, self.col, [x, y, wi, he], width=4)
             pygame.draw.rect(screen, color, [x+4, y+4, wi-8, he-8])
             if self.text_placement_specifier == 'text_input':
                 position = x + he//4, y + he//4
@@ -45,8 +65,25 @@ class Button():
             screen.blit(font.render(self.text, False,
                         THEME['def']), position)
 
+    def cycle_color(self):
+        # self.col = '#5750b3'
+        rgb_col = [0, 0, 0]
+        rgb_col[0] = int(math.sin(self.color_time*4)**2 * 200)
+        rgb_col[1] = int(math.cos(self.color_time*3)**2 * 180)
+        rgb_col[2] = int(math.cos(self.color_time*2)**2 * 210)
+        self.col = rgb_to_hex(rgb_col)
+        # print(self.col)
+        self.color_time += 5e-4
+        
+
     def hovering(self, pos):
         res = self.rect.collidepoint(pos) and self.is_active
+        if res:
+            self.dt = 0
+            self.cycle_color()
+        else:
+            self.dt = 1./240
+            # self.col = THEME['def']
         return res
 
 
